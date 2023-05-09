d3a0a2500dd71b68aa90ae9c2250e33d581e0b6e
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-09 11:44:50 +0800

    js element innerText

---
+++ c02/js/example.js
@@ -0,0 +1,26 @@
+let greeting = "Howdy";
+let my_name = "Robert";
+let message = ', please check you order:';
+let welcome = greeting + " " + my_name + message;
+let greetingElement = document.getElementById('greeting');
+greetingElement.innerText = welcome;
+
+let sign = "客家小館";
+let userSign_element = document.getElementById('userSign');
+userSign_element.innerText = sign;
+
+let tiles = sign.length;
+let tiles_element = document.getElementById('tiles');
+tiles_element.innerText=String(tiles);
+
+let subTotal = tiles*5;
+let subTotal_element = document.getElementById('subTotal');
+subTotal_element.innerText = '$' + String(subTotal);
+
+let shipping = 7;
+let shipping_element = document.getElementById('shipping')
+shipping_element.innerText = '$' + String(shipping);
+
+let grandTotal = shipping + subTotal;
+let grandTotal_element = document.getElementById('grandTotal')
+grandTotal_element.innerText = '$' + String(grandTotal);
