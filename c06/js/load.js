0ed8c91a02fed1ab3c6a1747f80708bdaf3586fa
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-15 16:52:16 +0800

    up0515

--- c06/js/load.js
+++ c06/js/load.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', (event) => {
     console.log("網頁已經載入")
     let usernameElement = document.getElementById('username')
     usernameElement.focus()
