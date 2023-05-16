80d3659c0d6785fe1d0ec4350e524a78f413718a
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-16 09:18:31 +0800

    mousemove position

--- c06/js/focus-blur.js
+++ c06/js/focus-blur.js
@@ -1,20 +1,3 @@
-// document.getElementById('username').addEventListener('focus', (event) => {
-//     let feedbackElement = document.getElementById('feedback');
-//     feedbackElement.className = 'tip';
-//     feedbackElement.innerText = 'username最少要5個字元';
-// });
-
-// document.getElementById('username').addEventListener('blur', (event) => {
-//     let feedbackElement = document.getElementById('feedback');
-//     let usernameElement = event.currentTarget;
-//     if(usernameElement.value.length < 5){
-//         feedbackElement.className = 'warning';
-//         feedbackElement.innerText = 'username最少要5個字元';
-//     }else{
-//         feedbackElement.innerText = '';
-//     }
-// );
-    
 document.getElementById('username').addEventListener('focus', (event) => {
     let feedbackElement = document.getElementById('feedback');
     feedbackElement.className = 'tip';
@@ -23,11 +6,28 @@ document.getElementById('username').addEventListener('focus', (event) => {
 
 document.getElementById('username').addEventListener('blur', (event) => {
     let feedbackElement = document.getElementById('feedback');
-    let usernameElement = event.currentTarget
-    if (usernameElement.value.length < 5) {
+    let usernameElement = event.currentTarget;
+    if(usernameElement.value.length < 5){
         feedbackElement.className = 'warning';
         feedbackElement.innerText = 'username最少要5個字元';
-    } else {
+    }else{
         feedbackElement.innerText = '';
     }
-})
+});
+    
+// document.getElementById('username').addEventListener('focus', (event) => {
+//     let feedbackElement = document.getElementById('feedback');
+//     feedbackElement.className = 'tip';
+//     feedbackElement.innerText = 'username最少要5個字元';
+// });
+
+// document.getElementById('username').addEventListener('blur', (event) => {
+//     let feedbackElement = document.getElementById('feedback');
+//     let usernameElement = event.currentTarget
+//     if (usernameElement.value.length < 5) {
+//         feedbackElement.className = 'warning';
+//         feedbackElement.innerText = 'username最少要5個字元';
+//     } else {
+//         feedbackElement.innerText = '';
+//     }
+// })
