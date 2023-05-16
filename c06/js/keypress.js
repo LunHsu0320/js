4e486ead1aa47a6266a7002e61ff22c3ade52c16
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-16 09:40:24 +0800

    keyup

---
+++ c06/js/keypress.js
@@ -0,0 +1,11 @@
+let messageElement = document.getElementById('message')
+let lastkeyElement = document.getElementById('lastKey')
+let charDisplay = document.getElementById('charactersLeft')
+messageElement.addEventListener('keyup', (event) => {
+    lastkeyElement.textContent = `最後輸入的key:${event.key},
+    keycode:${event.code}`;
+    let textEntered = messageElement.value
+    let counter = 180-textEntered.length
+    charDisplay.textContent = `還剩下:${counter}個字`
+    
+})
