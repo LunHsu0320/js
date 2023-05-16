fb3676c138c4fed6dfec6569f4089dc08866394d
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-16 14:09:21 +0800

    ajax lesson1

--- c06/js/form.js
+++ c06/js/form.js
@@ -1,10 +1,29 @@
 let formElement = document.getElementById('formSignup')
 let packageElement = document.getElementById('package')
-formElement.addEventListener('submit',(event)=>{
+let packageHintElement = document.getElementById('packageHint')
+let termsElement = document.getElementById('terms')
+let termsHintElement = document.getElementById('termsHint')
+
+formElement.addEventListener('submit', (event) => {
     console.log('submit')
-    event.preventDefault()
-    
+    if (!termsElement.checked) {
+        termsHintElement.innerText = '必須打勾同意'
+        event.preventDefault()
+    }
 })
-packageElement.addEventListener('change',(event)=>{
+
+packageElement.addEventListener('change', (event) => {
     console.log('change')
-})
+    // for(const option of packageElement.options){
+    //     console.log(option.label)
+    //     console.log(option.value)
+    // }
+    let selectedIndex = packageElement.selectedIndex
+    if (packageElement.options[selectedIndex].value == 'monthly') {
+        packageHintElement.innerHTML = '如果選擇1年可以省10元美金'
+    } else {
+        packageHintElement.innerHTML = '正確選擇'
+    }
+
+}
+)
