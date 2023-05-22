efc3c40737e6fa0e45942ea9368e62ea9ccc9b35
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-22 11:59:17 +0800

    0522up

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,20 +1,52 @@
 let sarea_array = []
 let sareaElement = document.getElementById("sarea");
+let areaNameElement = document.getElementById('areaName')
+let tbodyElement = document.getElementById('tbody')
+let youbikedata
 
 //select裡面的option被運用 當區域被選擇的時候 
 //顯示資料有沒有在列表裡 如果有顯示"有這個區域"
 sareaElement.addEventListener("change", (event) => {
     let selectedIndex = sareaElement.selectedIndex;
     selectedValue = sareaElement.options[selectedIndex].value;
-    if(sarea_array.includes(selectedValue)){
-        console.log('有這個區域')    
+    if (sarea_array.includes(selectedValue)) {
+        console.log('有這個區域')
+        console.log(`行政區:${selectedValue}`)
+        areaNameElement.innerText = selectedValue
+        let trHTML = "" //行政區內的html資訊
+        youbikedata.forEach(element => {
+
+            if (element.sarea == selectedValue) {
+                //站點狀態判斷
+                if (element.act == "1") {
+                    var status = "營業中"
+                } else { var status = "維護中" }
+                trHTML += "<tr>"
+                trHTML += "<td>" + element.sna.slice(11) + "</td>"
+                //slice()可以複製開始與結束點（結束點不算）中的內容
+                trHTML += "<td>" + element.ar + "</td>"
+                trHTML += "<td>" + element.tot + "</td>"                
+                trHTML += "<td>" + element.sbi + "</td>"
+                trHTML += "<td>" + element.bemp + "</td>"
+                trHTML += "<td>" + element.updateTime + "</td>"
+                trHTML += "<td>" + status + "</td>"
+                trHTML += "</tr>"
+            }
+
+        });
+        tbodyElement.innerHTML = trHTML
     }
 })
 
 //把讀取的資料整理並存入到html select裡
 function reqListener() {
-    let youbikedata = JSON.parse(this.responseText);
-    
+    console.log(this.status)
+    if(this.status!=200){
+        console.log("網頁維護中")
+        return
+    }
+    youbikedata = JSON.parse(this.responseText);
+
     for (const youbike of youbikedata) {
         sarea_array.push(youbike.sarea);
     }
@@ -23,22 +55,30 @@ function reqListener() {
     let optionElement = document.createElement("option");
     optionElement.textContent = '請選擇行政區'
     sareaElement.appendChild(optionElement);
- 
+
     //將讀取到的區名加入到select中    
     for (const area of sarea_array) {
         let optionElement = document.createElement("option");
         optionElement.textContent = area;
+        optionElement.setAttribute('value', area)
         sareaElement.appendChild(optionElement);
     }
 }
+
+function reqError(){
+    console.log("網頁維護中")
+}
+
 //建立XMLHttpRequest 下載資料load後執行reqListener
 const windowload = (event) => {
     console.log('page loaded')
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
-    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+    req.addEventListener("error",reqError);//偵測錯誤
+    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate123456.json");
     req.send();
 }
 
 //網頁load後執行windowload
 window.addEventListener('load', windowload)
+// window.addEventListener("error",reqError);
