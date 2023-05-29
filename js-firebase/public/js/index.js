4a28ccae112eb13e89afa2c4cb492f70bca5f6ca
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-29 14:40:48 +0800

    firebase app js import

--- js-firebase/public/js/index.js
+++ js-firebase/public/js/index.js
@@ -1,3 +1,25 @@
+// import { initializeApp } from "firebase/app";
+// import { initializeApp } from "../node_modules/firebase/app";
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js'
+// import { initializeApp } from '/js-firebase/node_modules/firebase/firebase-app.js'
+
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ_OmmFd7rpNiP3h-9hbJg4OCmtRfI4PM",
+  authDomain: "js-firebase-da216.firebaseapp.com",
+  projectId: "js-firebase-da216",
+  storageBucket: "js-firebase-da216.appspot.com",
+  messagingSenderId: "618826833406",
+  appId: "1:618826833406:web:b2821fada03fb390bb34af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 let formElement = document.querySelector('#form')
 let allOfDataArray = [];
 
