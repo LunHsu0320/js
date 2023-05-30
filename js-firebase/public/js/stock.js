c4924b46d2acc40038a70fe2ba4b5b8f1fd31a73
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-30 15:55:10 +0800

    stock 資料解析輸入圖表

--- js-firebase/public/js/stock.js
+++ js-firebase/public/js/stock.js
@@ -25,5 +25,35 @@ fromElement.addEventListener('submit', (event) => {
         return
     }
     console.log(stockId)
+
+    let monthElement = document.querySelector('input[type = "month"]')
+    let fullDateString = monthElement.value
+    fullDateString = fullDateString.replace("-","")
+    fullDateString += "01"
+    let url=`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${fullDateString}&stockNo=${stockId}`
     
+    console.log(url)
+
+    fetch(new Request(url),{'mode':'cors'})
+    .then((response) => {
+        return response.json()
+    }).then((r) => {
+        console.log(r)
+        let tbodyElement = document.querySelector('#tbody')
+        let rowHtmlString = ""
+        r.data.forEach(array => {
+            rowHtmlString += `<tr>`
+            rowHtmlString += `<td>${array[0]}</td>`
+            rowHtmlString += `<td>${array[1]}</td>`
+            rowHtmlString += `<td>${array[2]}</td>`
+            rowHtmlString += `<td>${array[3]}</td>`
+            rowHtmlString += `<td>${array[4]}</td>`
+            rowHtmlString += `<td>${array[5]}</td>`
+            rowHtmlString += `<td>${array[6]}</td>`
+            rowHtmlString += `<td>${array[7]}</td>`
+            rowHtmlString += `<td>${array[8]}</td>`
+            rowHtmlString += `</tr>`
+    })
+    tbodyElement.innerHTML = rowHtmlString
+})
 })
